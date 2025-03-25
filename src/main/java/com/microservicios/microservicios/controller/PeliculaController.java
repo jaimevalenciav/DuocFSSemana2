38f8c9747ceeb28249204536dda6402fd6a6e30a
Jaime Valencia <jvalencia@MBP-de-Jaime.bbrouter>
2025-03-24 21:35:56 -0300

    pequeña corrección se eliminan dependencias que no se utilizan en el controller

--- src/main/java/com/microservicios/microservicios/controller/PeliculaController.java
+++ src/main/java/com/microservicios/microservicios/controller/PeliculaController.java
@@ -11,9 +11,7 @@ import com.microservicios.microservicios.service.PeliculaService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.Optional;
 import java.util.List;
 
 @RestController
